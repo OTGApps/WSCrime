647cf74db1e2fe2f852fecc411a358f87beb6bdd
Mark Rickert <mjar81@gmail.com>
2013-06-13 09:19:05 -0400

    Make detailscreen a PM:TableScreen

--- app/screens/detail_screen.rb
+++ app/screens/detail_screen.rb
@@ -1,14 +1,13 @@
-class DetailScreen < ProMotion::Screen
+class DetailScreen < PM::TableScreen
 
   attr_accessor :parentVC, :data, :date
 
+  def on_load
+    @data.sort! {|a,b| a.annotation_params[:sort_by] <=> b.annotation_params[:sort_by] }
+  end
+
   def will_appear
-
     @view_setup ||= begin
-
-     @data.sort! { |a,b| a.sortableTime <=> b.sortableTime }
-
-
       self.setTitle("Details", subtitle:"for #{@date}")
       self.navigationController.navigationBar.barStyle = self.navigationController.toolbar.barStyle = UIBarStyleBlack
       self.navigationController.setToolbarHidden(false)
@@ -21,77 +20,58 @@ class DetailScreen < ProMotion::Screen
         line_break_mode: UILineBreakModeTailTruncation,
         font: UIFont.boldSystemFontOfSize(18),
         minimum_font_size: 10,
-        autoresizingMask: UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin,
+        resize: [:width, :left, :right],
         text: "#{incidentCount} Incidents & #{arrestCount} Arrests"
       }
-
       item = UIBarButtonItem.alloc.initWithCustomView(@label)
-      self.toolbarItems = [item];
+      self.toolbarItems = [item]
 
       # Done Button
-      set_nav_bar_right_button "Done", action: :close_modal, type: UIBarButtonItemStyleDone
+      set_nav_bar_button :right,
+        title: "Done",
+        action: :close_modal,
+        type: UIBarButtonItemStyleDone
 
-      @table = UITableView.alloc.initWithFrame(view.frame, style: UITableViewStylePlain)
-      @table.dataSource = self
-      @table.delegate = self
-
-      self.view = @table
+      update_table_data
     end
   end
 
-  def tableView(tableView, cellForRowAtIndexPath:indexPath)
-    cell = tableView.dequeueReusableCellWithIdentifier(cell_identifier)
-    if not cell
-      cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleSubtitle, reuseIdentifier:cell_identifier)
+  def table_data
+    [{cells:build_cells}]
+  end
+
+  def build_cells
+    c = []
+    @data.each_with_index do |cell,i|
+      c << {
+        title: cell.annotation_params[:title],
+        cell_style: UITableViewCellStyleSubtitle,
+        subtitle: cell.annotation_params[:subtitle],
+        selection_style: UITableViewCellSelectionStyleBlue,
+        accessory_type: UITableViewCellAccessoryDetailDisclosureButton,
+        accessory_action: :tapped_crime,
+        action: :tapped_crime,
+        arguments: {marker_index: i},
+        image: pin_image(cell)
+      }
     end
-
-    cell.imageView.image = UIImage.imageNamed( @data[indexPath.row].pinImage )
-    cell.selectionStyle = UITableViewCellSelectionStyleBlue
-    cell.textLabel.text = "#{@data[indexPath.row].title}"
-    cell.detailTextLabel.text = "#{@data[indexPath.row].subtitle}"
-    cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton
-
-    return cell
+    c
   end
 
-  def cell_identifier
-    @@cell_identifier ||= 'Cell'
+  def pin_image(cell)
+    cell.annotation_params[:type].downcase == "arrest" ? "pinannotation_red" : "pinannotation_purple"
   end
 
-  def tableView(tableView, numberOfRowsInSection: section)
-    case section
-    when 0
-      @data.length
-    else
-      0
-    end
-  end
-
-  # Tap on table Row
-  def tableView(tableView, didSelectRowAtIndexPath:indexPath)
-    tappedCrime( @data[indexPath.row] )
-  end
-
-  # Tap on row accessory
-  def tableView(tableView, accessoryButtonTappedForRowWithIndexPath:indexPath)
-    tappedCrime( @data[indexPath.row] )
-  end
-
-
-  def tappedCrime(marker)
-    unless @parentVC.nil?
-      @parentVC.closeDetailAndZoomToEvent(marker)
-    end
+  def tapped_crime(params = {})
+    @parentVC.closeDetailAndZoomToEvent(@data[params[:marker_index]]) unless @parentVC.nil?
   end
 
   def incidentCount
-    incidents = @data.select { |crime| crime.type == "Incident" }
-    incidents.length
+    @data.select { |crime| crime.annotation_params[:type] == "Incident" }.count
   end
 
   def arrestCount
-    arrests = @data.select { |crime| crime.type == "Arrest" }
-    arrests.length
+    @data.select { |crime| crime.annotation_params[:type] == "Arrest" }.count
   end
 
   def close_modal
